48f7b745d963efe1aeba9113f61a4b4f907798c6
Jackie Han <jackiehank@163.com>
2025-07-23 15:24:16 +0800

    chore: disable debug mode in production

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def serve_file(filename):
     return send_from_directory(ZOTERO_STORAGE, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
