163f527a478c99144d8a8b6885da0b939ad5166a
Jackie Han <jackiehank@163.com>
2025-08-01 12:53:33 +0800

    chore: disable debug mode in production

--- app.py
+++ app.py
@@ -155,4 +155,4 @@ def system_info():
     return jsonify(get_system_info())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
